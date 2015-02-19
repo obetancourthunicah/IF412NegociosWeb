deb48ea88ea804362d2d307695b56c175d737e61
Orlando Betancourth <obetancourth@unicah.edu>
2015-02-18 19:27:59 -0600

    Ejercicio 6 Mysql Skeleton

---
+++ ej6.php
@@ -0,0 +1,73 @@
+<?php
+  $registros = array();
+
+  if(isset($_POST["btnIns"])){
+    $registro = array();
+    $registro["codigo"] = 0;
+    $registro["descripcion"] = $_POST["txtDsc"];
+    $registro["lugar"] = $_POST["txtLgr"];
+    $registro["fecha"] = $_POST["txtFch"];
+    $registro["contactemail"] = $_POST["txtCrr"];
+    $registro["status"] = $_POST["txtSts"];
+
+    //Realizar la conexion con MySQL
+    //Preparar el Insert Statement
+    //Ejecutar el Insert Statement
+    //Obtener el último codigo generado
+    //Obtener los registros de la tabla
+  }
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8"/>
+    <title>Primer Formulario con MySQL Backend</title>
+  </head>
+  <body>
+    <h1>Charlas</h1>
+    <form action="ej6.php" method="POST">
+        <label for="txtDsc">Descripción</label>
+        <input type="text" name="txtDsc" id="txtDsc" />
+        <br/>
+        <label for="txtLgr">Lugar</label>
+        <input type="text" name="txtLgr" id="txtLgr" />
+        <br/>
+        <label for="txtFch">Fecha</label>
+        <input type="date" name="txtFch" id="txtFch" />
+        <br/>
+        <label for="txtCrr">Correo de Contacto</label>
+        <input type="email" name="txtCrr" id="txtCrr" />
+        <br/>
+        <label for="txtSts">Estado</label>
+        <select name="txtSts" id="txtSts">
+            <option value="PND">Pendiente</option>
+            <option value="CNF">Confirmado</option>
+            <option value="CNL">Cancelado</option>
+        </select>
+        <br/>
+        <input type="submit" name="btnIns" value="Grabar" />
+    </form>
+    <div>
+      <h2>Datos</h2>
+      <table>
+        <tr>
+          <th>Codigo</th>
+          <th>Descripción</th>
+          <th>Lugar</th>
+          <th>Fecha</th>
+          <th>Correo</th>
+          <th>Estado</th>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </table>
+    </div>
+  </body>
+</html>
